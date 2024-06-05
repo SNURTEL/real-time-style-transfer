24cb9b2129d6cff028a02687284ee5350416ca41
towienko <01169218@pw.edu.pl>
2024-06-05 05:03:33 +0200

    Reformat

--- src/test/test_utils.cpp
+++ src/test/test_utils.cpp
@@ -1,39 +1,49 @@
+#include "common/utils.hpp"
 #include <ATen/core/TensorBody.h>
 #include <catch2/catch_test_macros.hpp>
-#include "common/utils.hpp"
 #include <complex>
+#include <iostream>
 #include <opencv2/core/mat.hpp>
 #include <torch/torch.h>
-#include <iostream>
 #include <torch/types.h>
 
-TEST_CASE("Test tensor to cv2 conversion") { 
-    at::IntArrayRef in_size = { 1, 3, 2, 2 };
-    at::Tensor in = at::zeros(in_size, {torch::kU8});  
+TEST_CASE("Test tensor to cv2 conversion") {
+    at::IntArrayRef in_size = {1, 3, 2, 2};
+    at::Tensor in = at::zeros(in_size, {torch::kU8});
     cv::Mat out = tensorToCv2(in);
     cv::Size required_size = cv::Size(2, 2);
     REQUIRE(out.size() == required_size);
     REQUIRE(out.channels() == 3);
 }
 
-TEST_CASE("Test tensor to cv2 conversion copy") { 
-    at::IntArrayRef in_size = { 1, 3, 2, 2 };
-    at::Tensor in = at::zeros(in_size, {torch::kU8});  
+TEST_CASE("Test tensor to cv2 conversion copy") {
+    at::IntArrayRef in_size = {1, 3, 2, 2};
+    at::Tensor in = at::zeros(in_size, {torch::kU8});
     cv::Mat out = tensorToCv2(in, true);
-    REQUIRE(static_cast<unsigned long>(std::abs(out.data - static_cast<uchar*>(in.data_ptr()))) >= in.numel() * torch::elementSize(torch::typeMetaToScalarType(in.dtype())));
-    REQUIRE(static_cast<unsigned long>(std::abs(out.data - static_cast<uchar*>(in.data_ptr()))) >= out.total() * out.elemSize());
+    REQUIRE(static_cast<unsigned long>(
+                std::abs(out.data - static_cast<uchar *>(in.data_ptr()))) >=
+            in.numel() *
+                torch::elementSize(torch::typeMetaToScalarType(in.dtype())));
+    REQUIRE(static_cast<unsigned long>(
+                std::abs(out.data - static_cast<uchar *>(in.data_ptr()))) >=
+            out.total() * out.elemSize());
 }
 
-TEST_CASE("Test cv2 to tensor conversion") { 
+TEST_CASE("Test cv2 to tensor conversion") {
     cv::Mat in = cv::Mat::ones(2, 2, CV_8UC3);
     at::Tensor out = cv2ToTensor(in);
-    at::IntArrayRef required_size = { 1, 3, 2, 2 };
+    at::IntArrayRef required_size = {1, 3, 2, 2};
     REQUIRE(out.sizes() == required_size);
 }
 
-TEST_CASE("Test cv2 to tensor conversion copy") { 
+TEST_CASE("Test cv2 to tensor conversion copy") {
     cv::Mat in = cv::Mat::ones(2, 2, CV_8UC3);
     at::Tensor out = cv2ToTensor(in, true);
-    REQUIRE(static_cast<unsigned long>(std::abs(in.data - static_cast<uchar*>(out.data_ptr()))) >= out.numel() * torch::elementSize(torch::typeMetaToScalarType(out.dtype())));
-    REQUIRE(static_cast<unsigned long>(std::abs(in.data - static_cast<uchar*>(out.data_ptr()))) >= in.total() * in.elemSize());
+    REQUIRE(static_cast<unsigned long>(
+                std::abs(in.data - static_cast<uchar *>(out.data_ptr()))) >=
+            out.numel() *
+                torch::elementSize(torch::typeMetaToScalarType(out.dtype())));
+    REQUIRE(static_cast<unsigned long>(
+                std::abs(in.data - static_cast<uchar *>(out.data_ptr()))) >=
+            in.total() * in.elemSize());
 }
